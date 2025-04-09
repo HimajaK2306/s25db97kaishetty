96614061965dec3e59d951781c854553dc9cc89b
Himaja2306 <himajakaishetty1@gmail.com>
2025-04-09 08:32:07 -0500

    new changes

---
+++ controllers/quarries.js
@@ -0,0 +1,21 @@
+var Costume = require('../models/costume');
+// List of all Costumes
+exports.quarries_list = function(req, res) {
+res.send('NOT IMPLEMENTED: quarries list');
+};
+// for a specific Costume.
+exports.quarries_list_detail = function(req, res) {
+res.send('NOT IMPLEMENTED: quarries detail: ' + req.params.id);
+};
+// Handle Costume create on POST.
+exports.quarries_create_post = function(req, res) {
+res.send('NOT IMPLEMENTED: quarries create POST');
+};
+// Handle Costume delete from on DELETE.
+exports.quarries_delete = function(req, res) {
+res.send('NOT IMPLEMENTED: quarries delete DELETE ' + req.params.id);
+};
+// Handle Costume update form on PUT.
+exports.quarries_update_put = function(req, res) {
+res.send('NOT IMPLEMENTED: quarries update PUT' + req.params.id);
+};
---
+++ routes/resource.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+// Require controller modules.
+var api_controller = require('../controllers/api');
+var costume_controller = require('../controllers/costume');
+/// API ROUTE ///
+// GET resources base.
+router.get('/', api_controller.api);
+/// COSTUME ROUTES ///
+// POST request for creating a Costume.
+router.post('/quarries', quarries_controller.quarries_create_post);
+// DELETE request to delete Costume.
+router.delete('/quarries/:id', quarries_controller.quarries_delete);
+// PUT request to update Costume.
+router.put('/quarries/:id', quarries_controller.quarries_update_put);
+// GET request for one Costume.
+router.get('/quarries/:id', quarries_controller.quarries_detail);
+// GET request for list of all Costume items.
+router.get('/quarries', quarries_controller.quarries_list);
+module.exports = router;
