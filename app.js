c2f21a4d4a23c5eb59bb14ce20c621ecced6f0bb
Himaja2306 <himajakaishetty1@gmail.com>
2025-04-17 01:14:37 -0500

    new changes

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,12 +24,41 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat', // you can change this secret to anything secure
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 require('dotenv').config();
 const connectionString = process.env.MONGO_CONN
 mongoose = require('mongoose');
 mongoose.connect(connectionString);
+
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username })
+  .then(function (user){
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  })
+  .catch(function(err){
+  return done(err)
+  })
+  })
+  )
 
 // We can seed the collection if needed on server start
 async function recreateDB(){
@@ -84,6 +115,15 @@ app.get('/gridbuild', function(req, res) {
   // Pass the parsed numbers to the template
   res.render('grid', { title: "Grid Display", query: { rows, cols } });
 });
+
+
+var Account = require('./models/account');
+
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
