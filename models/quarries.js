d81137e2c16171993304c6f42a8035f35a25ce32
Himaja2306 <himajakaishetty1@gmail.com>
2025-04-23 18:11:28 -0500

    new changes

--- models/quarries.js
+++ models/quarries.js
@@ -1,7 +1,22 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
 const quarriesSchema = mongoose.Schema({
-quarries_type: String,
-depth_meters: Number,
-material: String
-})
-module.exports = mongoose.model("quarries",quarriesSchema)
+  quarries_type: {
+    type: String,
+    required: true,
+    minLength: [3, "Quarry type must be at least 3 characters long"],
+    maxLength: [30, "Quarry type cannot exceed 30 characters"]
+  },
+  depth_meters: {
+    type: Number,
+    required: true,
+    min: [5, "Depth must be at least 5 meters"],
+    max: [500, "Depth cannot exceed 500 meters"]
+  },
+  material: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("quarries", quarriesSchema);
--- routes/quarries.js
+++ routes/quarries.js
@@ -7,6 +7,16 @@ router.get('/', function(req, res, next) {
 });
 
 
+// A little function to check if we have an authorized user and continue on
+// or redirect to login.
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  res.redirect("/login");
+};
+
+
 // var express = require('express');
 const quarries_controllers= require('../controllers/quarries');
 var router = express.Router();
@@ -16,9 +26,11 @@ router.get('/quarries/:id', quarries_controllers.quarries_list_detail);
 //router.get('/quarries/:id', quarries_controllers.quarries_update_put);
 //router.delete('/quarries/:id', quarries_controller.quarries_delete);
 router.get('/detail', quarries_controllers.quarries_view_one_Page);
-router.get('/create', quarries_controllers.quarries_create_Page);
-router.get('/update', quarries_controllers.quarries_update_Page);
-router.get('/delete', quarries_controllers.quarries_delete_Page);
+router.get('/create',secured, quarries_controllers.quarries_create_Page);
+router.get('/update',secured, quarries_controllers.quarries_update_Page);
+router.get('/delete',secured, quarries_controllers.quarries_delete_Page);
+
+
 
 module.exports = router;
 
